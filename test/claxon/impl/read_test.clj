c9d697f43d0076e837c1d5badbfd7868bead9197
Rahul De <rahul080327@gmail.com>
2026-06-20 22:48:38 +0100

    [tests] clean

--- test/claxon/impl/read_test.clj
+++ test/claxon/impl/read_test.clj
@@ -1,7 +1,8 @@
 (ns claxon.impl.read-test
   (:require
-   [clojure.test :refer [deftest testing is]]
-   [claxon.impl.read :as ir])
+   [claxon.conf :as conf]
+   [claxon.impl.read :as ir]
+   [clojure.test :refer [deftest is testing]])
   (:import
    [java.io ByteArrayInputStream EOFException]))
 
@@ -14,42 +15,7 @@
   ^ByteArrayInputStream [^String s]
   (ByteArrayInputStream. (.getBytes s "UTF-8")))
 
-(def default-shapes
-  "A representative subset of the real frame-shapes table, mirroring
-   claxon.conf/defaults closely enough to exercise the parser end-to-end."
-  {"INFO" {:args [{:name :info :type :json}]}
-   "CONNECT" {:args [{:name :opts :type :json}]}
-   "PUB" {:args [{:name :subject :type :str}
-                 {:name :reply-to :type :str :optional true}
-                 {:name :bytes :type :int}]
-          :payloads [{:name :body :type :bytes :length :bytes}]}
-   "HPUB" {:args [{:name :subject :type :str}
-                  {:name :reply-to :type :str :optional true}
-                  {:name :hdr-bytes :type :int}
-                  {:name :bytes :type :int}]
-           :payloads [{:name :headers :type :headers :length :hdr-bytes}
-                      {:name :body :type :bytes :length [:- :bytes :hdr-bytes]}]}
-   "MSG" {:args [{:name :subject :type :str}
-                 {:name :sid :type :str}
-                 {:name :reply-to :type :str :optional true}
-                 {:name :bytes :type :int}]
-          :payloads [{:name :body :type :bytes :length :bytes}]}
-   "HMSG" {:args [{:name :subject :type :str}
-                  {:name :sid :type :str}
-                  {:name :reply-to :type :str :optional true}
-                  {:name :hdr-bytes :type :int}
-                  {:name :bytes :type :int}]
-           :payloads [{:name :headers :type :headers :length :hdr-bytes}
-                      {:name :body :type :bytes :length [:- :bytes :hdr-bytes]}]}
-   "SUB" {:args [{:name :subject :type :str}
-                 {:name :queue-group :type :str :optional true}
-                 {:name :sid :type :str}]}
-   "UNSUB" {:args [{:name :sid :type :str}
-                   {:name :max-msgs :type :int :optional true}]}
-   "PING" {}
-   "PONG" {}
-   "+OK" {}
-   "-ERR" {:args [{:name :msg :type :str}]}})
+(def default-shapes (:claxon/frame-shapes (conf/defaults)))
 
 ;; ---------------------------------------------------------------------------
 ;; read-all
